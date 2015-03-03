fb22bf435b3e338da18880c2346a2e4a3234db52
David P. Kendal <dpkendal@dpk.org.uk>
2015-03-03 15:15:02 +0100

    eval site v. 1

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, make_response
+import evaluator
+
+app = Flask(__name__)
+
+@app.route("/")
+def evaluate():
+    code = request.query_string
+
+    result = evaluator.run(code)
+    if not result.strip():
+        result = '(no output)'
+    
+    return make_response(result, 200, {'Content-Type': 'text/plain'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ evaluator.py
@@ -0,0 +1,65 @@
+import base64
+import chardet
+import codecs
+import collections
+import copy
+import csv
+import datetime
+import encodings
+import feedparser
+import functools
+import getopt
+import glob
+import gzip
+import hashlib
+import html
+import io
+import itertools
+import lxml.etree
+import lxml.html
+import mailbox
+import mimetypes
+import operator
+import os
+import pytz
+import quopri
+import random
+import re
+import requests
+import json
+import socket
+import string
+import sys
+import textwrap
+import time
+import unicodedata
+import urllib
+import xml
+import zlib
+import lxml.etree as etree
+from lxml.html import html5parser
+
+def run(code):
+    old_stdout = sys.stdout
+    old_stderr = sys.stderr
+    
+    output = io.StringIO()
+    sys.stdout = output
+    sys.stderr = output
+    
+    try:
+        try:
+            output.write(str(eval(code)))
+        except SyntaxError:
+            exec(code)
+        
+        output.seek(0)
+        return output.readline()
+    except Exception as error:
+        if str(error):
+            return "%s: %s" % (type(error).__name__, str(error))
+        else:
+            return type(error).__name__
+    finally:
+        sys.stdout = old_stdout
+        sys.stderr = old_stderr
